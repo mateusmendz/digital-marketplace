98baa636573eb06ac4912689842ee40e66fe4efe
wopeads <wopeads@gmail.com>
2021-11-17 03:36:40 -0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
 
+const projectId = "7261cc553fd445abb4f275293176adba";
+
 module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
     },
     mumbai: {
-      url: "https://polygon-mumbai.infura.io/v3/7261cc553fd445abb4f275293176adba",
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [],
     },
-    mainnet: {},
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+    },
   },
   solidity: "0.8.4",
 };
